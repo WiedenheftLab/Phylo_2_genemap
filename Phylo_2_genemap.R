aa0ae45fd3f3fa4a5a753503faeba8bdabe1d765
WiedenheftLab <wiedenheft_lab@montana.edu>
2023-05-26 00:54:05 -0600

    first commit

---
+++ Phylo_2_genemap.R
@@ -0,0 +1,119 @@
+
+### Check package availability ###
+
+list.of.packages <- c("ape", "ggtree","tidyverse","gggenes","tidytree")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+if("ggtree" %in% new.packages){
+  if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  
+  BiocManager::install("ggtree")
+}
+
+### Load packages ###
+
+suppressMessages(suppressWarnings(library("ape")))
+suppressMessages(suppressWarnings(library("ggtree")))
+suppressMessages(suppressWarnings(library("tidyverse")))
+suppressMessages(suppressWarnings(library("gggenes")))
+suppressMessages(suppressWarnings(library("tidytree")))
+
+### Parse Arguments ###
+
+args <- commandArgs(trailingOnly = TRUE)
+
+#args <- c("--tree","/Users/muratbuyukyorukmsu/Desktop/ANK/final_tree.nexus","--genemap","/Users/muratbuyukyorukmsu/Desktop/ANK/Pro_ANK_containing_hits_genemap_inuse.txt","--anchor","ANK_1", "--clade", "/Users/muratbuyukyorukmsu/Desktop/ANK/test_for_R.txt","--output","outfile.txt")
+
+filename <- args[which(args=="--tree")+1]
+genemap_in <-  args[which(args=="--genemap")+1]
+anc_point <-  args[which(args=="--anchor")+1]
+clade_in <-  args[which(args=="--clade")+1]
+file.out <- args[which(args=="--output")+1]
+
+### Import Tree ###
+
+treein <- read.tree(filename)
+
+x <- treein
+p1 <- ggtree(x)
+
+### Color branches if Clade info provided ###
+
+if(length(clade_in)!=0){
+  quants <- list()
+  
+  clade_df <- read.delim(clade_in, header = TRUE)
+  clade_append <- NULL
+  
+  for(i in 1:ncol(clade_df)){
+    clade_info <- paste(as.list(clade_df[,i][clade_df[,i]!=""]),collapse = "|")
+    assign(paste("clade",i,sep = "_"),grep(clade_info,treein$tip.label))
+    if(i==1){
+      clade_append <- clade_info
+    }else{
+      clade_append <- paste(clade_append,clade_info,sep = "|")
+    }
+    if(i==ncol(clade_df)){
+      uncladed <- grep(clade_append,treein$tip.label,invert = TRUE)
+    }
+  }
+  for(i in 1:ncol(clade_df)){
+    quants[["Rest"]] <- uncladed
+    quants[[paste0(colnames(clade_df)[i])]] <- get(paste("clade",i,sep = "_"))
+  }
+  
+  p1 <- groupOTU(p1, quants, 'Clade') + aes(color=Clade)
+}
+
+### Define genemap function ###
+
+get_genes <- function(data, genome) {
+  filter(data, molecule == genome) %>% pull(gene)
+}
+
+### Import genemap dataframe ###
+
+genemap <- read.delim(genemap_in,header = TRUE)
+
+### Generate genemap matrix (takes a long time when the df is long) ###
+
+g <- unique(genemap[,1])
+n <- length(g)
+d <- matrix(nrow = n, ncol = n)
+rownames(d) <- colnames(d) <- g
+genes <- lapply(g, get_genes, data = genemap)
+
+for (i in 1:n) {
+  for (j in 1:i) {
+    jaccard_sim <- length(intersect(genes[[i]], genes[[j]])) / 
+      length(union(genes[[i]], genes[[j]]))
+    d[j, i] <- d[i, j] <- 1 - jaccard_sim
+  }
+}
+
+### Map domains to ggtree plot ###
+
+p <- p1 + 
+  geom_facet(mapping = aes(xmin = start, xmax = end, fill = gene),
+             data = genemap, geom = geom_motif, panel = 'Alignment',
+             on = anc_point ,arrowhead_height = unit(0, "mm"), arrowhead_width = unit(0, "mm"), arrow_body_height = unit(0.1,"mm"),size = 0) + 
+  scale_x_continuous(expand=c(0,0)) #+ theme_genes()
+
+p <- facet_widths(p, widths=c(1,3))
+
+setEPS()
+postscript(paste0(tools::file_path_sans_ext(file.out),".eps"))
+Sys.sleep(2)
+p
+dev.off()
+
+pdf(paste0(tools::file_path_sans_ext(file.out),".pdf"))
+Sys.sleep(2)
+p
+dev.off()
+
+print(paste0("Raw plot is exported as PDF and SVG files and can be found in ",getwd()))
+
+
---
+++ README.md
@@ -0,0 +1,71 @@
+# Phylo_2_genemap
+
+Author: Murat Buyukyoruk
+
+Associated lab: Wiedenheft lab
+
+        Phylo_2_genemap help:
+
+This script is developed to illustrate a phylogenetic tree (.newick) along side with a genemap that demonstrates 
+the protein sequence alignment and domain prevelance.
+
+R is required to generate plots with various CRAN packages (i.e., ggtree, gggenes, tidyverse, tidytree and their 
+depencencies). The R script is generated to install CRAN packages if they were not available in the system, 
+
+Syntax:
+
+        python Phylo_2_genemap.py -t demo.nexus -o demo_tree_genemap -g genemap_dataframe.txt -a ANK_1 -c 
+clade_info.txt
+
+Example Dataframe for the genemap_dataframe.txt file (tab separated excel file is required):
+
+        molecule	gene	start	end	strand	orientation
+        NC_002967.9_2504	Length	1	934	forward	1
+        NC_002967.9_2504	ANK_1	65	97	forward	1
+        NC_002967.9_2504	ANK	247	312	forward	1
+        NC_002967.9_2504	ANK	313	346	forward	1
+        NC_002967.9_2504	ANK	347	380	forward	1
+        NC_002967.9_2504	ANK	381	413	forward	1
+        NC_002967.9_2504	ANK	543	575	forward	1
+        NC_002967.9_2504	ANK	813	845	forward	1
+
+Example Dataframe for the clade_info.txt file (tab separated excel file is required):
+
+        Clade1	Clade2
+        NZ_OX366336.1_932	NZ_CP025544.1_57
+        NZ_OX366337.1_584	
+        NZ_OX366343.1_632	
+        NZ_OX366401.1_611	
+        NZ_CP076228.1_128	
+        NZ_JAATLD010000001.1_206	
+        NZ_CP069053.1_339	
+        NZ_VCEF01000099.1_2	
+        NZ_VCEG01000064.1_7	
+        NC_012416.1_91	
+        NZ_CP042904.1_88	
+        NZ_JAGKTH010000088.1_15	
+        NZ_OX366369.1_350	
+
+Phylo_2_genemap dependencies:
+
+	R                                                       refer to 
+https://rstudio-education.github.io/hopr/starting.html
+
+Input Paramaters (REQUIRED):
+----------------------------
+	-i/--input		NEWICK			Specify a phylogenetic tree in .newick format.
+
+	-o/--output		Output file	    Specify a output file name for PNG and EPS files.
+
+	-g/--genemap	Dataframe		Specify a genemap file that include the doamin name, start, stop, 
+strand and orientation information for each domain occurs in protein accessions.
+
+    -a/--anchor     gene_name       Specify a gene name for aligning the proteins like an anchoring point.
+	
+	-c/--clade      Dataframe       Specify a dataframe that includes the clades to color on phylogenetic 
+tree. 
+
+Basic Options:
+--------------
+	-h/--help		HELP			Shows this help text and exits the run.
+
